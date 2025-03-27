77377c831e1d6a1ef63d0cccdd3c50e990ecf8a0
Gary <gary@jgerickson.net>
2025-03-26 22:08:15 -0400

    Initial Commit

---
+++ WhatIsFootballToYou/WhatIsFootballToYouApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatIsFootballToYouApp.swift
+//  WhatIsFootballToYou
+//
+//  Created by Gary Erickson on 3/26/25.
+//
+
+import SwiftUI
+
+@main
+struct WhatIsFootballToYouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
